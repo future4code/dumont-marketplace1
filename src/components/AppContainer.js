59745c6ff90e9bce5179d59dab4640d3bda0a60c
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2020-11-05 09:38:43 -0300

    Carousel pronto

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+/*Componentes*/
+import Carousel from './Carousel/Carousel';
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+        <Carousel />
       </div>
     )
   }
